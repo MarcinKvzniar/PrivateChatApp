c5334afba69dd5ba7cc4a81af5ae4fd40326d81b
Domin C <dominikcedro00@gmail.com>
2024-11-18 13:40:32 +0100

    ADD login functionality to the views.py using simple jwt authentication.

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -3,9 +3,19 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+
+class LoginView(TokenObtainPairView):
+    """
+    This view handles the login functionality.
+    It uses the TokenObtainPairView from SimpleJWT to generate JWT tokens.
+    """
+    pass
