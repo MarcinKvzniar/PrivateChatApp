8dff20e2b93ee82831fd1b3b911189912b5bcb16
Maksym Kashka <276488@student.pwr.edu.pl~>
2024-11-25 06:04:38 +0100

    Added InvitePage, RequestPage, and AnswerPage, but App and index not ok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import InvitePage from './pages/InvitePage';
+import RequestPage from './pages/RequestPage';
+import AnswerPage from './pages/AnswerPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Private Chat App</h1>
+      <InvitePage />
+      <RequestPage />
+      <AnswerPage />
     </div>
   );
-}
+};
 
 export default App;
