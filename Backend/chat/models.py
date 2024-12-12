780a2231e994e541ffe62f9cc04f0dabab307707
Domin C <dominikcedro00@gmail.com>
2024-12-12 21:28:55 +0100

    UPDATE fixed changes according to jira ticket PRIV-62

--- Backend/chat/models.py
+++ Backend/chat/models.py
@@ -1,9 +1,14 @@
+"""
+author: Dominik Cedro
+"""
 from django.db import models
 from django.contrib.auth.models import User
 
 class Chat(models.Model):
     chat_id = models.AutoField(primary_key=True)
     chat_name = models.CharField(max_length=255)
+    creator = models.ForeignKey(User, related_name='created_chats', on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='chats')
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Message(models.Model):
